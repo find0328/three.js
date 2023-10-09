ef70472ad0fc82fcce1b44395fca9f9251c084b6
siyoutao <siyoutao@bytedance.com>
2023-10-09 21:03:52 +0800

    fixup: add vrplayer.
    delete sphere pic and use mark room
    上个版本中手柄是黑色的且没有射线

--- siyt/vite-project/src/utils/VRVideo.js
+++ siyt/vite-project/src/utils/VRVideo.js
@@ -28,23 +28,22 @@ function init() {
   document.body.appendChild( VRButton.createButton( renderer ) );
 
   // 2>
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
+  // camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1100 );
+  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 10 );
+  camera.position.set( 0, 1, 3 );
   scene = new THREE.Scene();
   
   // 2.1> 图片球形贴图，box映射贴图见webxr_vr_panorama.html
-  const geometry = new THREE.SphereGeometry( 50, 60, 10 );
-  geometry.scale( - 1, 1, 1 ); // invert the geometry on the x-axis so that all of the faces point inward
-  const texture = new THREE.TextureLoader().load( './files/snow_hdr.jpg' );
-  texture.colorSpace = THREE.SRGBColorSpace;
-  const material = new THREE.MeshBasicMaterial( { map: texture } );
-  const mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
+  // addSpherePic();
+  addMarkRoom();
 
   // 2.2> 视频
   videoPlayerObject = new PlaneVideoPlayer({
     source: "./files/coffee.mp4",
     play_btn_color: 0x6EABDD
   });
+  videoPlayerObject.getMesh().position.x = 0;
+  videoPlayerObject.getMesh().position.y = 1;
   videoPlayerObject.getMesh().position.z = -2;
   scene.add(videoPlayerObject.getMesh());
 
@@ -52,9 +51,38 @@ function init() {
   raycaster = new THREE.Raycaster();
 
   // 2.4> 加载左右手柄射线
+  function onSelectStart() {
+    this.userData.isSelecting = true;
+  }
+  function onSelectEnd() {
+    this.userData.isSelecting = false;
+    if ( INTERSECTION ) {
+      const offsetPosition = { x: - INTERSECTION.x, y: - INTERSECTION.y, z: - INTERSECTION.z, w: 1 };
+      const offsetRotation = new THREE.Quaternion();
+      const transform = new XRRigidTransform( offsetPosition, offsetRotation );
+      const teleportSpaceOffset = baseReferenceSpace.getOffsetReferenceSpace( transform );
+      renderer.xr.setReferenceSpace( teleportSpaceOffset );
+    }
+  }
   controller1 = renderer.xr.getController( 0 );
+  controller1.addEventListener( 'selectstart', onSelectStart );
+  controller1.addEventListener( 'selectend', onSelectEnd );
+  controller1.addEventListener( 'connected', function ( event ) {
+    this.add( buildController( event.data ) );
+  } );
+  controller1.addEventListener( 'disconnected', function () {
+    this.remove( this.children[ 0 ] );
+  } );
   scene.add( controller1 );
   controller2 = renderer.xr.getController( 1 );
+  controller2.addEventListener( 'selectstart', onSelectStart );
+  controller2.addEventListener( 'selectend', onSelectEnd );
+  controller2.addEventListener( 'connected', function ( event ) {
+    this.add( buildController( event.data ) );
+  } );
+  controller2.addEventListener( 'disconnected', function () {
+    this.remove( this.children[ 0 ] );
+  } );
   scene.add( controller2 );
 
   // 2.5> 加载左右手柄模型
@@ -69,6 +97,44 @@ function init() {
   window.addEventListener( 'resize', onWindowResize );
 }
 
+function addSpherePic() {
+  const geometry = new THREE.SphereGeometry( 500, 60, 40 );
+  geometry.scale( - 1, 1, 1 ); // invert the geometry on the x-axis so that all of the faces point inward
+  const texture = new THREE.TextureLoader().load( './files/snow_hdr.jpg' );
+  texture.colorSpace = THREE.SRGBColorSpace;
+  const material = new THREE.MeshBasicMaterial( { map: texture } );
+  const mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+}
+
+function addBoxPic() {
+}
+
+function addMarkRoom() {
+  room = new THREE.LineSegments(
+    new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ).translate( 0, 3, 0 ),
+    new THREE.LineBasicMaterial( { color: 0xbcbcbc } )
+  );
+  scene.add( room );
+  scene.add( new THREE.HemisphereLight( 0xa5a5a5, 0x898989, 3 ) );
+
+  const light = new THREE.DirectionalLight( 0xffffff, 3 );
+  light.position.set( 1, 1, 1 ).normalize();
+  scene.add( light );
+
+  marker = new THREE.Mesh(
+    new THREE.CircleGeometry( 0.25, 32 ).rotateX( - Math.PI / 2 ),
+    new THREE.MeshBasicMaterial( { color: 0xbcbcbc } )
+  );
+  scene.add( marker );
+
+  floor = new THREE.Mesh(
+    new THREE.PlaneGeometry( 4.8, 4.8, 2, 2 ).rotateX( - Math.PI / 2 ),
+    new THREE.MeshBasicMaterial( { color: 0xbcbcbc, transparent: true, opacity: 0.25 } )
+  );
+  scene.add( floor );
+}
+
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
@@ -80,26 +146,26 @@ function animate() {
 }
 
 function render() {
-  // INTERSECTION = undefined;
-  // if ( controller1.userData.isSelecting === true ) {
-  //   tempMatrix.identity().extractRotation( controller1.matrixWorld );
-  //   raycaster.ray.origin.setFromMatrixPosition( controller1.matrixWorld );
-  //   raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-  //   const intersects = raycaster.intersectObjects( [ floor ] );
-  //   if ( intersects.length > 0 ) {
-  //     INTERSECTION = intersects[ 0 ].point;
-  //   }
-  // } else if ( controller2.userData.isSelecting === true ) {
-  //   tempMatrix.identity().extractRotation( controller2.matrixWorld );
-  //   raycaster.ray.origin.setFromMatrixPosition( controller2.matrixWorld );
-  //   raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-  //   const intersects = raycaster.intersectObjects( [ floor ] );
-  //   if ( intersects.length > 0 ) {
-  //     INTERSECTION = intersects[ 0 ].point;
-  //   }
-  // }
-  // if ( INTERSECTION ) marker.position.copy( INTERSECTION );
-  // marker.visible = INTERSECTION !== undefined;
+  INTERSECTION = undefined;
+  if ( controller1.userData.isSelecting === true ) {
+    tempMatrix.identity().extractRotation( controller1.matrixWorld );
+    raycaster.ray.origin.setFromMatrixPosition( controller1.matrixWorld );
+    raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
+    const intersects = raycaster.intersectObjects( [ floor ] );
+    if ( intersects.length > 0 ) {
+      INTERSECTION = intersects[ 0 ].point;
+    }
+  } else if ( controller2.userData.isSelecting === true ) {
+    tempMatrix.identity().extractRotation( controller2.matrixWorld );
+    raycaster.ray.origin.setFromMatrixPosition( controller2.matrixWorld );
+    raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
+    const intersects = raycaster.intersectObjects( [ floor ] );
+    if ( intersects.length > 0 ) {
+      INTERSECTION = intersects[ 0 ].point;
+    }
+  }
+  if ( INTERSECTION ) marker.position.copy( INTERSECTION );
+  marker.visible = INTERSECTION !== undefined;
 
   renderer.render( scene, camera );
 }
